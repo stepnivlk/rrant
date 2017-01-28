bcd2c833c40489933f639d39e76177b3f3361f1a
Tomas Koutsky <tomas@stepnivlk.net>
2017-01-28 23:42:43 +0000

    add output spec

---
+++ spec/rrant/output_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'rrant_helper'
+
+describe Rrant::Output do
+  context '#in' do
+    let(:rant) { RrantHelper.fake_rant(1, false, true) }
+    let(:output_obj) {
+      described_class.new(rant, false)
+    }
+
+    it 'returns rant as a hash' do
+      expect(output_obj.in).to include(rant)
+    end
+  end
+
+  context '#out without image' do
+    let(:rant) { RrantHelper.fake_rant(1, false, true) }
+    let(:output_obj) {
+      described_class.new(rant, false)
+    }
+
+    it 'puts rant and image to STDOUT' do
+      expect(STDOUT).to receive(:puts).with(rant['text'])
+      output_obj.out
+    end
+  end
+
+  context '#out with image' do
+    let(:rant) { RrantHelper.fake_rant(1, false, true) }
+    let(:output_obj) {
+      described_class.new(rant, true)
+    }
+
+    it 'puts rant and image to STDOUT' do
+      28.times { expect(STDOUT).to receive(:puts) }
+      output_obj.out
+    end
+  end
+end
--- spec/rrant_helper.rb
+++ spec/rrant_helper.rb
@@ -7,11 +7,15 @@ module RrantHelper
     FileUtils.rm_rf(root_path + '/.rrant')
   end
 
-  def self.fake_rant(id, viewed = false)
-    {
-      'id' => id,
-      'text' => '640kb should be enough'
-    }.tap { |rant| rant['viewed_at'] = DateTime.now if viewed }
+  def self.fake_rant(id, viewed = false, image = false)
+    rant = { 'id' => id,
+             'text' => '640kb should be enough' }
+    image_path = File.expand_path('../files/bill.jpg', File.dirname(__FILE__))
+
+    rant.tap do |r|
+      r['viewed_at'] = DateTime.now if viewed
+      r['image'] = image_path if image
+    end
   end
 
   def self.fake_placeholder
